a594b234ebca31f56a41876c932abd6e3336ce38
ShornikovNazar <nazar.shornikov04@gmail.com>
2024-10-28 18:38:15 +0000

    Faulty version_2

--- lab_4.cpp
+++ lab_4.cpp
@@ -5,8 +5,8 @@ int main()
 {
     setlocale(LC_ALL, ".1251");
     double y, x;
-    cout<<"Введіть значення х\n";
-    cin>>x;
+    <<"Введіть значення х\n"
+    cin<<x
     y=((4*x*x-2)*(x+2)/(2*x+3))+(x*x*sin(x))/(2*x+1);
     cout<<y;
     return 0;
